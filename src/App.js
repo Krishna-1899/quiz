db15cab298066cb5461cdb90eab4afc25637cb59
Krishna-1899 <rajeshbhanushali189@gmail.com>
2023-05-31 18:48:38 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { useState } from 'react';
+import Quiz from './Components/Quiz'; 
+function App() {
+return (
+  <Quiz/>
+)
+};
+
+export default App;
---
+++ src/Questionfile.js
@@ -0,0 +1,29 @@
+export const quiz = {
+    totalQuestions:4,
+    perQuestionScore:1,
+    questions : [
+        {
+            question:
+            'What is the capital of France?',
+            choices:['America','Melbourne','Paris','Sydney'],
+            correctAnswer : 'Paris'
+        },
+        {
+            question:
+            'The name of the galaxy in which the earth planet is',
+            choices:['Andromeda','Ursa major','Ursa minor','Milky way'],
+            correctAnswer:'Milky way'
+        },
+        {
+            question:
+            'Which of the following Planets have no moon',
+            choices:['Pulto and Mercury','Mars and Venus','Mercury and Venus','Pluto and Mars'],
+            correctAnswer:'Mercury and Venus'
+        },
+        {
+            question:'The Black hole is',
+            choices:['A dark hollow cavity','A massive collapsing star','The other side of the moon','The other side of sun'],
+            correctAnswer:'A massive collapsing star'
+        }
+    ]
+}
